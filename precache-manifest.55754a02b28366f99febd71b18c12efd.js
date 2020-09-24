d3c92530393c01ad6c462d461d680111b8e4eb43
rmit-s3734938-vineet-bugtani <s3734938@student.rmit.edu.au>
2020-09-24 16:00:32 +1000

    Updates

---
+++ precache-manifest.55754a02b28366f99febd71b18c12efd.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ccb576c8d09bcaa6e02a9a6db0378c0b",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "1f8f8d877d39623d8b8c",
+    "url": "/react-portfolio/static/css/2.ad257b39.chunk.css"
+  },
+  {
+    "revision": "22d30157ff343c4680d9",
+    "url": "/react-portfolio/static/css/main.2ed8960c.chunk.css"
+  },
+  {
+    "revision": "1f8f8d877d39623d8b8c",
+    "url": "/react-portfolio/static/js/2.40449f67.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/react-portfolio/static/js/2.40449f67.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "22d30157ff343c4680d9",
+    "url": "/react-portfolio/static/js/main.3bea979a.chunk.js"
+  },
+  {
+    "revision": "aff33cef9dbb8787d6b3",
+    "url": "/react-portfolio/static/js/runtime-main.213b1880.js"
+  },
+  {
+    "revision": "6907bf0f1825d2c70964b27f7f230e74",
+    "url": "/react-portfolio/static/media/bank.6907bf0f.jpg"
+  },
+  {
+    "revision": "00d967dc4ae8351e51e0a08efc454f56",
+    "url": "/react-portfolio/static/media/burger.00d967dc.png"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/react-portfolio/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/react-portfolio/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/react-portfolio/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/react-portfolio/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/react-portfolio/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "b244c8097fb5875f28ccb99bedfb4873",
+    "url": "/react-portfolio/static/media/jemena.b244c809.png"
+  },
+  {
+    "revision": "bec60248a46241aa4dc86b83ec380537",
+    "url": "/react-portfolio/static/media/rpc.bec60248.jpg"
+  }
+]);
