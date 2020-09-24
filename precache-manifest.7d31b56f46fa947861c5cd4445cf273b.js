baa73c43ce2f9409b6e282e7da12a55967d4e682
rmit-s3734938-vineet-bugtani <s3734938@student.rmit.edu.au>
2020-09-24 16:07:08 +1000

    Updates

---
+++ precache-manifest.7d31b56f46fa947861c5cd4445cf273b.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0c263bac05f5869c265ea6d90d1f1eb",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "d06be27ab8451c43dc79",
+    "url": "/portfolio/static/css/2.dcf46461.chunk.css"
+  },
+  {
+    "revision": "658418f6d65af1629ddf",
+    "url": "/portfolio/static/css/main.2ed8960c.chunk.css"
+  },
+  {
+    "revision": "d06be27ab8451c43dc79",
+    "url": "/portfolio/static/js/2.fbd88c0f.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/portfolio/static/js/2.fbd88c0f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "658418f6d65af1629ddf",
+    "url": "/portfolio/static/js/main.34d764f7.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "6907bf0f1825d2c70964b27f7f230e74",
+    "url": "/portfolio/static/media/bank.6907bf0f.jpg"
+  },
+  {
+    "revision": "00d967dc4ae8351e51e0a08efc454f56",
+    "url": "/portfolio/static/media/burger.00d967dc.png"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/portfolio/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/portfolio/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/portfolio/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/portfolio/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/portfolio/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "b244c8097fb5875f28ccb99bedfb4873",
+    "url": "/portfolio/static/media/jemena.b244c809.png"
+  },
+  {
+    "revision": "bec60248a46241aa4dc86b83ec380537",
+    "url": "/portfolio/static/media/rpc.bec60248.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-portfolio/precache-manifest.55754a02b28366f99febd71b18c12efd.js"
+  "/portfolio/precache-manifest.7d31b56f46fa947861c5cd4445cf273b.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-portfolio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
