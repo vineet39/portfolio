2b9c543f6e431f967b8f3b053f1d223321f31ab4
rmit-s3734938-vineet-bugtani <s3734938@student.rmit.edu.au>
2020-11-10 18:41:01 +1100

    Minor profile fix

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -5,7 +5,7 @@ import classNames from './Profile.css';
 const profile = () => {
     const details = {
         intro: "My name is Vineet. I am a...",
-        profile: "Analytically minded and collaborative individual desiring the job of a software developer. Bringing expertise in writing both front-end and back-end code with a willingness to learn new technologies and embrace challenging tasks at work.",
+        profile: "Analytically minded and collaborative individual desiring the job of a software developer. I would be bringing to the table experience in writing full-stack code with a willingness to learn new technologies and embrace challenging tasks at work.",
     };
 
     return (
