726d91261e8440707c74a3b911a8cbf45c59372b
vineet39 <vinit.bugtani@gmail.com>
2020-11-27 00:21:04 +1100

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.dbaa8bcac6bab6bb17b851c9deb037e7.js"
+  "/portfolio/precache-manifest.c5efb397a721b6a1b50f56a9fc984f4e.js"
 );
 
 self.addEventListener('message', (event) => {
