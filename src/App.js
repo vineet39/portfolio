4ab6de4613f92f3c09b66031efdd2fa0826b0db6
vineet39 <vinit.bugtani@gmail.com>
2021-01-03 00:56:45 +1100

    Removing metadata

--- src/App.js
+++ src/App.js
@@ -19,10 +19,6 @@ export default class App extends Component {
       <Aux>
         <Helmet>
           <html lang="en" />
-          <meta name="keywords" content="Vineet, Bugtani, RMIT, Portfolio, Software, Developer, Profile, Personal, Website, Github, Graduate, Masters, IT, Information, Technology, Application, App, Engineer" />
-          <meta name="description" content="Vineet Bugtani's personal portfolio. Graduate software developer actively seeking a job in the software industry." />
-          <meta name="author" content="Vineet Bugtani" />
-          <meta name="google-site-verification" content="YHzQSrlf8B4xrNccsH6r0Qx3iP5SJ4H6AdwIYIKwpUw" />
           <title>Vineet Bugtani</title>
         </Helmet>
         <ScrollToTop />
